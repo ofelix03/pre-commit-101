91e4bc68495da0cd1593803d6cce629df40ec417
Felix Otoo <ofelix03@gmail.com>
2022-01-18 23:33:24 +0000

    update main.py file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-test = "test-version"
-print(f"Hello world revised {test}")
+"""
+Module docstring provided
+"""
+TEST = "test-version"
+print(f"Hello world revised {TEST}")
 
 
 def say_name(name="Felix"):
+    """
+    Function docstring provided
+    """
     print(f"My nameis {name}")
 
 
