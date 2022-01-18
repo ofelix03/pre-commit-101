d17a377371bb4b4d4836586b64245e3cb10472a4
Felix Otoo <ofelix03@gmail.com>
2022-01-18 23:20:34 +0000

    update main.py file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import os
-
-print("hello world revised")
+test = "test-version"
+print(f"Hello world revised {test}")
 
 
 def say_name(name="Felix"):
-    print(f"My nameis ${name}")
+    print(f"My nameis {name}")
 
 
 say_name()
